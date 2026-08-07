f02ba9626640d12496e415db3b6d7c5d992ab62d
Vasanth2126 <vasanthvh2126@gmail.com>
2026-08-07 17:02:05 +0530

    Create 10.Regular_Expression_Matching.java

---
+++ string/10.Regular_Expression_Matching.java
@@ -0,0 +1,12 @@
+import java.util.regex.*;
+class Solution {
+    public boolean isMatch(String s, String p) {
+        Pattern P=Pattern.compile(p);
+        Matcher M=P.matcher(s);
+        if(M.matches())
+        {
+            return true;
+        }
+        return false;
+    }
+}
