529e2621f606fd02c183f1d8235a397ea10f4337
Vasanth2126 <vasanthvh2126@gmail.com>
2026-08-13 17:32:51 +0530

    Create 322.Coin_Change.java

---
+++ arrays/322.Coin_Change.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][]dp=new int[coins.length+1][amount+1];
+
+        for(int i=1;i<=amount;i++)
+        {
+            dp[0][i]=amount+1;
+        }
+
+        for(int i=1;i<=coins.length;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+                dp[i][j]=dp[i-1][j];
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=Math.min(dp[i][j],1+dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+        return dp[coins.length][amount]==amount+1?-1:dp[coins.length ][amount];
+    }
+}
