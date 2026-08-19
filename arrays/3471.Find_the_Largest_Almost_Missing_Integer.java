18a2ad3fc8a26f42fb2ce27734129afa955668f7
Vasanth2126 <vasanthvh2126@gmail.com>
2026-08-19 17:10:47 +0530

    Create 3471.Find_the_Largest_Almost_Missing_Integer.java

---
+++ arrays/3471.Find_the_Largest_Almost_Missing_Integer.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        int[]a=new int[nums.length];
+        ArrayList<ArrayList<Integer>>mainlist=new ArrayList<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<k;i++)
+        {
+            list.add(nums[i]);
+        }
+        mainlist.add(list);
+        int i=1,j=k;
+        while(j<nums.length)
+        {
+            ArrayList<Integer>list1=new ArrayList<>();
+            for(int x=i;x<=j;x++)
+            {
+                list1.add(nums[x]);
+            }
+            mainlist.add(new ArrayList<>(list1));
+            i++;
+            j++;
+        }
+      //  list.clear();
+        for(i=0;i<nums.length;i++)
+        {
+            int c=0;
+            for(j=0;j<mainlist.size();j++)
+            {
+             list=mainlist.get(j);
+            if(list.contains(nums[i]))
+            {
+                c++;
+            }
+            }
+            a[i]=c;
+        }
+        int ans=-1;
+        for(i=0;i<a.length;i++)
+        {
+            if(a[i]==1)
+            {
+                ans=Math.max(ans,nums[i]);
+            }
+        }
+        // System.out.print(mainlist);
+         return ans;
+    }
+}
